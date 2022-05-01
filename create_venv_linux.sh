68ba843fb43d9fb650097d89f4b996fb994e0f5c
locobastos <bastien.thierry.martin@gmail.com>
2022-05-01 17:40:12 +0200

    Add venv scripts

---
+++ create_venv_linux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Checks if Python is installed
+if ! command -v python3 &>/dev/null
+then
+    echo "Python 3 is not installed, please install Python first..."
+    exit 1
+fi
+
+# Creates the Linux virtual environment if not exists
+if ! [ -d venv_linux ]
+then
+    echo "Creating the Linux virtual environment..."
+    python3 -m venv venv_linux
+fi
+
+# Prepares the virtual environment
+. venv_linux/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
